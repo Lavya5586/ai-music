c536111f7412eb49efe9be580f2afe463f3577e4
Lavya5586 <78597821+Lavya5586@users.noreply.github.com>
2021-10-19 17:38:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 its_ok = "";
-monday = ";"
+monday = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     its_ok = loadSound("Imagine-Dragons-It-s-Ok.mp3");
@@ -12,13 +18,31 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " +leftWristX+ "LeftWristY = " +leftwristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " +rightWristX+ "RightWristY = " +rightWristY);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is inialized');
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
-}
-
-function play() {
-    its_ok.play();
-    monday.play();
 }
